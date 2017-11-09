2240c14ed1a52842261abe03146bc2d4aa138b42
root <root@localhost.localdomain>
2017-11-08 22:52:02 -0500

    Updated

--- push.sh
+++ push.sh
@@ -1,4 +1,4 @@
 git add .
 git commit -m" Updated $1"
-git push https://github.com/BoazHalter/Containerised-Pipe.git 
+git push https://BoazHalter:boaz286455github.com/BoazHalter/Containerised-Pipe.git 
 
