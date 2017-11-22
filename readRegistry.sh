a1c9cdad64b13c86483cd7fd9961f2b105cfb132
root <root@localhost.localdomain>
2017-11-22 15:48:01 -0500

    Updated

--- readRegistry.sh
+++ readRegistry.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/bash
-registry="10.0.0.16:5000"
-repos=`curl http://$registry/v2/_catalog?n=100 | jq '.repositories[]' | tr -d '"'`
-for repo in $repos; do 
-   curl http://$registry/v2/$repo/tags/list; 
-done
+
+function Connect()
+{
+  registry="10.0.0.16:5000"
+  repos=`curl http://$registry/v2/_catalog?n=100 | jq '.repositories[]' | tr -d '"'`
+  for repo in $repos; do 
+     curl http://$registry/v2/$repo/tags/list; 
+  done
+}
+
+
+output=$(Connect)
+
+for i in ${output[@]}; do echo $i|grep $1|grep -w $2 ; done 
